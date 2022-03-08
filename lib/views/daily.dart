706c9fea069ee5192134cbd6cfcb34c0b630c836
Dredayduncan <dredayduncan@gmail.com>
2022-03-08 13:59:16 +0000

    Created the profile, daily, and workoutTracker pages for further development

---
+++ lib/views/daily.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Daily extends StatefulWidget {
+  const Daily({Key? key}) : super(key: key);
+
+  @override
+  _DailyState createState() => _DailyState();
+}
+
+class _DailyState extends State<Daily> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
