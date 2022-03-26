b4427ef0b49a2e0c9207865e4d0c5b2d4de21dc4
paaks100 <pkkodua@gmail.com>
2022-03-26 18:46:30 +0000

    added firebase packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_inside_fitness/views/welcome_screen.dart';
 
-void main() => runApp(MaterialApp(home: WelcomeScreen()));
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(home: WelcomeScreen()));
+}
